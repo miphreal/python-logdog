35f290dc9cf267ac063325e274e992701ff3669f
Evgeny Lychkovsky <miphreal@gmail.com>
2015-07-05 01:07:52 +0300

    merged develop into master and bumped version

--- logdog/version.py
+++ logdog/version.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 2
-PATCH_VERSION = 9
+PATCH_VERSION = 10
 version = (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
 __version__ = '.'.join(map(str, version))
