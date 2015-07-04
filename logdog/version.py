44194b199f1906a156aa15bb97a40bb8b13f3d52
Evgeny Lychkovsky <miphreal@gmail.com>
2015-07-05 02:12:09 +0300

    reupload to pypi with new version

--- logdog/version.py
+++ logdog/version.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 2
-PATCH_VERSION = 10
+PATCH_VERSION = 11
 version = (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
 __version__ = '.'.join(map(str, version))
